677381b2e1d46a7eab516f9bf5fdb44bf7e1202b
giga.freijo <giga.freijo@aea5ac83-672e-0410-a297-69868726a4ee>
2007-04-18 20:29:31 +0000

    Primeras pruebas del grupo sobre RedBlack.

---
+++ trunk/redblack/Main.java
@@ -0,0 +1,24 @@
+package redblack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Map<Integer,List<Integer>> t = new TreeMap<Integer,List<Integer>>();
+
+		List<Integer> arr = new ArrayList<Integer>();
+		arr.add(1); arr.add(2);
+		
+		t.put(0,arr);
+		
+		System.out.println(t.get(0));
+		System.out.println(t.size());
+		System.out.println(t.toString());
+	}
+
+}
