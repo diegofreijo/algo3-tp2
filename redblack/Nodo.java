f0063d965d527dd064a5b95ff2a282eec7212c5e
giga.freijo <giga.freijo@aea5ac83-672e-0410-a297-69868726a4ee>
2007-04-18 20:29:31 +0000

    Primeras pruebas del grupo sobre RedBlack.

---
+++ redblack/Nodo.java
@@ -0,0 +1,10 @@
+package redblack;
+
+import java.awt.List;
+
+public class Nodo
+{
+	public Boolean es_negro;
+	public Integer pivote; 
+	public List<Intervalo> menores, mayores;
+}
---
+++ redblack/RedBlack.java
@@ -0,0 +1,28 @@
+package redblack;
+
+import java.awt.List;
+import java.util.TreeMap;
+
+public class RedBlack
+{
+	///////////////////////////////////
+	// Miebros privados
+	///////////////////////////////////
+	private RedBlack izq, der;
+	private Nodo nodo;
+	
+	
+	///////////////////////////////////
+	// Metodos publicos
+	///////////////////////////////////
+	public RedBlack()
+	{
+	}
+	
+	public void Insertar(Nodo b)
+	{
+		TreeMap a = new TreeMap();
+		a.
+	}
+	
+}
