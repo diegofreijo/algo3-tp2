bd60afb6a0b1fd84e95a27d93f35fd4b13a6db33
giga.freijo <giga.freijo@aea5ac83-672e-0410-a297-69868726a4ee>
2007-04-24 05:59:47 +0000

    Comence a hacer el parser y las clases ayudantes.

--- boobleart/Intervalo.java
+++ boobleart/Intervalo.java
@@ -4,4 +4,15 @@ public class Intervalo
 {
 	public int inicio;
 	public int fin;
+		
+	public Intervalo(long inicio, long fin)
+	{
+		this.inicio = inicio;
+		this.fin = fin;
+	}
+	
+	public String toString()
+	{
+		return String.valueOf(inicio) + " " + String.valueOf(fin);
+	}
 }
