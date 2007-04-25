a7636738d588509e0b1db1aaa523bc45a2ba3eb6
matiasblanco18 <matiasblanco18@aea5ac83-672e-0410-a297-69868726a4ee>
2007-04-25 03:09:47 +0000

    Se agrego la clase Rango y Listas, a modo de prolijidad para la creacion del Interval.
    Se cambiaron los parametros del Intervalo por int, ya que sino no se podia usar como clave para el TreeMap.
    
    Se siguio creando la creacion (cuak) pero todavia no esta terminado.

--- trunk/intervalTree/Nodo.java
+++ trunk/intervalTree/Nodo.java
@@ -1,21 +1,106 @@
 package intervalTree;
 
+import java.util.ArrayList;
 import java.util.List;
+import boobleart.Intervalo;
+
 
 public class Nodo
 {
-	public int pivot;
-	public List<List<Integer>> inicio;
-	public List<List<Integer>> fin;
-	public IntervalTree menores;
-	public IntervalTree mayores;
+	public long pivot;
+	public List<Intervalo> inicio;
+	public List<Intervalo> fin;
 	
-	public Nodo(int pivot, List<List<Integer>> inicio, List<List<Integer>> fin, IntervalTree menores, IntervalTree mayores)
+	public Nodo(long pivot, List<Intervalo> intervalos)
 	{
 		this.pivot = pivot;
-		this.inicio = inicio;
-		this.fin = fin;
-		this.mayores = mayores;
-		this.menores = menores;	
+		this.inicio = OrdenarX(intervalos);
+		this.fin = OrdenarY(intervalos);
 	}
+	
+	private List<Intervalo> OrdenarX(List<Intervalo> intervalos)
+    {
+		List<Intervalo> ret = new ArrayList<Intervalo>();
+		
+		if(intervalos.size() > 0)
+		{
+			Intervalo pivot = new Intervalo(0, 0);
+			pivot = intervalos.get(0);
+			
+			List<Intervalo> mayores = new ArrayList<Intervalo>();
+			List<Intervalo> menores = new ArrayList<Intervalo>();
+			
+			int i = 1;
+			
+			while(i < intervalos.size())
+			{
+				Intervalo actual = intervalos.get(i); 
+				if(actual.inicio < pivot.inicio)
+				{
+					menores.add(actual);					
+				} else {
+					mayores.add(actual);					
+				}
+				i++;
+			}
+			
+			if(mayores.size() > 0)
+			{
+				ret.addAll(OrdenarX(mayores));
+			}
+			
+			ret.add(pivot);
+			
+			if(menores.size() > 0)
+			{
+				ret.addAll(OrdenarX(menores));
+			}
+		} else {
+			ret = intervalos;
+		}
+	    return ret;
+    }
+	
+	private List<Intervalo> OrdenarY(List<Intervalo> intervalos)
+    {
+		List<Intervalo> ret = new ArrayList<Intervalo>();
+		
+		if(intervalos.size() > 0)
+		{
+			Intervalo pivot = new Intervalo(0, 0);
+			pivot = intervalos.get(0);
+			
+			List<Intervalo> mayores = new ArrayList<Intervalo>();
+			List<Intervalo> menores = new ArrayList<Intervalo>();
+			
+			int i = 1;
+			
+			while(i < intervalos.size())
+			{
+				Intervalo actual = intervalos.get(i); 
+				if(actual.fin < pivot.fin)
+				{
+					menores.add(actual);					
+				} else {
+					mayores.add(actual);					
+				}
+				i++;
+			}
+			
+			if(mayores.size() > 0)
+			{
+				ret.addAll(OrdenarY(mayores));
+			}
+			
+			ret.add(pivot);
+			
+			if(menores.size() > 0)
+			{
+				ret.addAll(OrdenarY(menores));
+			}
+		} else {
+			ret = intervalos;
+		}
+	    return ret;
+    }
 }
